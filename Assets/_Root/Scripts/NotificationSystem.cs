807627ccddea544e56fc8e6c2c4aa0209c68b6b5
Habel <tamikys15@gmail.com>
2022-12-06 23:50:22 +0300

    Add notification system

---
+++ Assets/_Root/Scripts/NotificationSystem.cs
@@ -0,0 +1,15 @@
+using System;
+using Unity.Notifications.Android;
+using UnityEngine;
+
+namespace _Root.Scripts
+{
+    public class NotificationSystem : MonoBehaviour
+    {
+        private AndroidNotificationChannel _defaultNotificationChanel = new AndroidNotificationChannel();
+        private void Start()
+        {
+            
+        }
+    }
+}
