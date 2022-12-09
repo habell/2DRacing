a1638d623953985f84ca19be34089cd176f12c7f
Habel <tamikys15@gmail.com>
2022-12-09 23:55:39 +0300

    New notification channel

--- Assets/_Root/Scripts/NotificationSystem.cs
+++ Assets/_Root/Scripts/NotificationSystem.cs
@@ -6,10 +6,18 @@ namespace _Root.Scripts
 {
     public class NotificationSystem : MonoBehaviour
     {
-        private AndroidNotificationChannel _defaultNotificationChanel = new AndroidNotificationChannel();
+        private AndroidNotificationChannel _defaultNotificationChanel;
+
         private void Start()
         {
-            
+            _defaultNotificationChanel = new AndroidNotificationChannel()
+            {
+                Id = "default_channel",
+                Name = "Default Chanel",
+                Description = "Test notification",
+                Importance = Importance.Default,
+            };
+            AndroidNotificationCenter.RegisterNotificationChannel(_defaultNotificationChanel);
         }
     }
 }
