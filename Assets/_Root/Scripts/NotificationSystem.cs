e6e2c5c443ed2b6f9b8d9f22bfcb5ece8dae4879
Habel <tamikys15@gmail.com>
2022-12-10 23:09:07 +0300

    Create new notifications

--- Assets/_Root/Scripts/NotificationSystem.cs
+++ Assets/_Root/Scripts/NotificationSystem.cs
@@ -8,6 +8,7 @@ namespace _Root.Scripts
     {
         private AndroidNotificationChannel _defaultNotificationChanel;
 
+        private int _id;
         private void Start()
         {
             _defaultNotificationChanel = new AndroidNotificationChannel()
@@ -18,6 +19,17 @@ namespace _Root.Scripts
                 Importance = Importance.Default,
             };
             AndroidNotificationCenter.RegisterNotificationChannel(_defaultNotificationChanel);
+
+            AndroidNotification notification = new AndroidNotification()
+            {
+                Title = "Test",
+                Text = "test text",
+                SmallIcon = "default",
+                LargeIcon = "default",
+                FireTime = DateTime.Now.AddSeconds(10),
+            };
+
+            _id = AndroidNotificationCenter.SendNotification(notification, "default_channel");
         }
     }
 }
