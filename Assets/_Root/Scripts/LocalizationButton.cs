d6d995720858286dda59ba6a4878664112c8c8e5
Habel <tamikys15@gmail.com>
2022-12-12 22:44:57 +0300

    Fix change language button problems

--- Assets/_Root/Scripts/LocalizationButton.cs
+++ Assets/_Root/Scripts/LocalizationButton.cs
@@ -2,30 +2,31 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
-using UnityEngine.UIElements;
-using Button = UnityEngine.UI.Button;
+using UnityEngine.UI;
 
 namespace _Root.Scripts
 {
-    [RequireComponent(typeof(TextMeshPro))]
     [RequireComponent(typeof(Button))]
     public class LocalizationButton : MonoBehaviour
     {
+        [SerializeField]
         private Button _changeLanguageButton;
         private int _currentIndex = 0;
 
         private void Start()
         {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_currentIndex];
             _changeLanguageButton = GetComponent<Button>();
-            _changeLanguageButton.onClick.AddListener((() => ChangeLanguage()));
+            _changeLanguageButton.onClick.AddListener(ChangeLanguage);
         }
 
         private void OnDestroy() => _changeLanguageButton.onClick.RemoveAllListeners();
 
         private void ChangeLanguage()
         {
-            if (_currentIndex > 0) _currentIndex = 1;
-            else _currentIndex = 0;
+            print(_currentIndex);
+            if (_currentIndex > 0) _currentIndex = 0;
+            else _currentIndex = 1;
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_currentIndex];
         }
     }
